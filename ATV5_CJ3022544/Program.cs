3a0fbba6a8e534f421be5950a3814dd3bb16372c
Nick Rafael <nicolas.rafael@aluno.ifsp.edu.br>
2024-03-27 11:05:13 -0300

    Adicionar arquivos de projeto.

---
+++ ATV5_CJ3022544/Program.cs
@@ -0,0 +1,177 @@
+﻿/*
+
+// exemplo:
+
+using System;
+
+int a, temp;
+int soma = 0;
+
+for (a = 0; a < 3; a++)
+{
+    Console.WriteLine("digite um valor");
+    temp = int.Parse(Console.ReadLine());
+
+    if (temp == 0)
+    {
+        break;
+    }
+
+    soma = soma + temp;
+}
+
+
+
+
+
+Console.WriteLine(" a media é {0}", soma / 3);
+
+// Atividade 1
+
+int n, cod;
+
+Console.WriteLine(" digite um numero inteiro");
+cod = int.Parse(Console.ReadLine());
+
+for (n = 0; n < cod; n++)
+{
+    Console.WriteLine(n);
+}
+
+// Atividade 2
+
+int num1, cod2, par;
+
+Console.WriteLine("digite um numero");
+cod2 = int.Parse(Console.ReadLine());
+
+for (par = 0; par <= cod2; par++)
+{
+    if ( par % 2 == 0)
+    {
+        Console.WriteLine(par);
+    }
+}
+
+
+// atividade 3
+
+int mil, att;
+
+Console.WriteLine("digite um numero");
+att = int.Parse(Console.ReadLine());
+
+for (mil = 1000; mil >= att; mil--)
+{
+    if (mil % 2 == 0)
+    {
+        Console.WriteLine(mil);
+    }
+}
+
+
+// atividade 4
+
+int num, cod;
+
+Console.WriteLine("digite um numero");
+num = int.Parse(Console.ReadLine());
+
+for (cod = 0; num > 0; cod += 200)
+{
+    if (num > 0) ;
+    {
+        Console.WriteLine(cod);
+    }
+
+  
+}
+*/
+
+// Atividade 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
